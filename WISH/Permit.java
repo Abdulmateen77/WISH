89abff5fc3c63211ec13b67518ee4f06dc3f2fd7
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-10 23:50:02 +0000

    Update Permit.java

--- WISH/Permit.java
+++ WISH/Permit.java
@@ -1,16 +1,27 @@
- 
 /**
- * A Permit has an id number, name, a luxury rating and number of credits.
+ * The Permit class represents a permit with an ID number, name, luxury rating, and number of credits.
  * 
- * @author (your name) 
+ * This class provides methods to retrieve and manipulate permit information.
+ * 
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Permit 
-{   private int permitId;
+{   
+    // Fields to store permit information
+    private int permitId;
     private String name;
     private int luxuryrating;
     private int noofcredits;
     
+    /**
+     * Constructor for creating a Permit object with specified values.
+     * 
+     * @param id The ID number of the permit.
+     * @param n The name associated with the permit.
+     * @param l The luxury rating of the permit.
+     * @param c The initial number of credits associated with the permit.
+     */
     public Permit( int id, String n, int l, int c )
     {
         permitId = id;
@@ -19,65 +30,96 @@ public class Permit
         noofcredits = c;
     }
     
+    /**
+     * Get the ID number of the permit.
+     * 
+     * @return The permit's ID number.
+     */
     public int getidnumber()
     { 
         return permitId;
-    
     }
     
+    /**
+     * Get the name associated with the permit.
+     * 
+     * @return The name associated with the permit.
+     */
     public String name()
     { 
         return name;
-    
     }
     
+    /**
+     * Get the luxury rating of the permit.
+     * 
+     * @return The luxury rating of the permit.
+     */
     public int getluxuryrating()
     { 
         return luxuryrating;
-    
     }
     
+    /**
+     * Get the current number of credits associated with the permit.
+     * 
+     * @return The number of credits associated with the permit.
+     */
     public int getcredits()
     { 
         return noofcredits;
-    
     }
     
+    /**
+     * Add credits to the permit.
+     * 
+     * @param newcredits The number of credits to add.
+     */
     public void addcredits(int newcredits)
     { 
         noofcredits = noofcredits + newcredits;
     }
     
+    /**
+     * Deduct credits from the permit.
+     * 
+     * @param subcredits The number of credits to deduct.
+     */
      public void deductcredits(int subcredits)
     { 
         noofcredits = noofcredits - subcredits;
     }
     
+    /**
+     * Check if the permit has enough credits (at least 3 credits).
+     * 
+     * @return True if there are enough credits, false otherwise.
+     */
     public boolean enoughcredit()
     {
-        if (noofcredits >= 3 )
-        {
-        return true;
-        }
-        else return false;
+        return (noofcredits >= 3);
     }
     
+    /**
+     * Provide a rating review based on the luxury rating.
+     * 
+     * @return A message indicating eligibility for a shuttle ride.
+     */
     public String ratingreview()
     {
         String i = "\n You are eligible to make a shuttle ride";
         String n = "\n Sorry you cannot ride";
-        if (luxuryrating >= 3 )
-        {
-        return i;
-        }
-        else return n;
+        return (luxuryrating >= 3) ? i : n;
     }
      
+    /**
+     * Get a string representation of the permit.
+     * 
+     * @return A string containing permit information.
+     */
     public String toString()
     {
-        String p;
-        p = "\nPermit ID Number: " + permitId + ","+ "   " + "Name "  + name + "  \n Luxury rating: "
-                                + luxuryrating + "  \nNumber of credits: " + noofcredits ;
-        return p;
+        return "\nPermit ID Number: " + permitId + "," + "   " + "Name "  + name +
+               "  \n Luxury rating: " + luxuryrating + "  \nNumber of credits: " + noofcredits ;
     }
 }
